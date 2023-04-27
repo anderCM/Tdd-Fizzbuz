2ce74d835fae9271ce30858ab8888354b50558c3
Feliverse <haybar@gmail.com>
2023-04-27 19:37:45 -0400

    Create Classes to Reverse and Fizzbuzz

--- classes/solver.rb
+++ classes/solver.rb
@@ -15,4 +15,16 @@ class Solver
     end
     factorial
   end
+
+  def reverse(word)
+    word.reverse
+  end
+
+  def fizzbuzz(number)
+    return 'fizzbuzz' if (number % 3).zero? && (number % 5).zero?
+    return 'fizz' if (number % 3).zero?
+    return 'buzz' if (number % 5).zero?
+
+    number.to_s
+  end
 end
